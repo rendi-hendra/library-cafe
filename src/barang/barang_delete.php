684d32cbafc005b04d82f4ba096fa3554ec5d752
babe25 <babehndr5@gmail.com>
2025-06-22 13:39:35 +0700

    feat(barang): Delete barang with gambar

--- src/barang/barang_delete.php
+++ src/barang/barang_delete.php
@@ -1,6 +1,9 @@
 <?php
 include '../../connectdb.php';
 $id = $_GET['id'];
+$query = mysqli_query($conn, "SELECT gambar FROM barang WHERE id=$id");
+$gambar = mysqli_fetch_assoc($query)['gambar'] ?? null;
+unlink('../../img/' . $gambar);
 $sql = "DELETE FROM barang WHERE id=$id";
 if (mysqli_query($conn, $sql)) {
     echo "Data berhasil dihapus.";
